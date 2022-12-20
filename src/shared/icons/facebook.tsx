3a08a0b29b7daa85c87bdda5f59146059396b7ae
Alina <ruesullivanpro@gmail.com>
2022-12-20 13:54:40 +0400

    chore: add icons

---
+++ src/shared/icons/facebook.tsx
@@ -0,0 +1,15 @@
+export const FacebookIcon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M18.4549 2.90918C15.2919 2.90918 12.7278 5.18855 12.7278 8.0003V11.6365H9.45557V16.0001H12.7278V29.0921H17.6367V16.0001H21.809L22.5453 11.6365H17.6367V8.0003C17.6367 7.59862 18.003 7.27299 18.4549 7.27299H22.5456V2.90918H18.4549Z"
+      fill="currentColor"
+    />
+  </svg>
+)
