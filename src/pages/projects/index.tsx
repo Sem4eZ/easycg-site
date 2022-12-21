6c7e4e87eb3eecfcc9e354898d7d1ed8f8861ba7
Alina <ruesullivanpro@gmail.com>
2022-12-21 09:26:25 +0400

    chore: add page template

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,8 +6,9 @@ const ProjectsPage = () => (
     title="work"
     decorationText={<NumberOutlined type="header">01</NumberOutlined>}
     subtitleContent="Block under the title"
-    filter="Filter"
-    children="Some page content"></Page>
+    filter="Filter">
+    "Some page content"
+  </Page>
 )
 
 export default ProjectsPage
