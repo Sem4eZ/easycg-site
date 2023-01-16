6309fa5527cdb85d85d45b77d142cec045f93f26
Alina <ruesullivanpro@gmail.com>
2023-01-16 19:08:25 +0400

    chore: put theme to the cookie

--- src/features/theme/theme-toggler/index.tsx
+++ src/features/theme/theme-toggler/index.tsx
@@ -1,5 +1,6 @@
 import IconButton from '@mui/material/IconButton'
 import { styled } from '@mui/material/styles'
+import Cookies from 'js-cookie'
 import { useEffect, useState } from 'react'
 
 import { MoonIcon } from 'shared/icons/moon'
@@ -7,14 +8,25 @@ import { getBreakpointsStylesByArray } from 'shared/lib/get-breakpoints-styles-b
 import { useGetUserTime } from 'shared/lib/use-get-time'
 import { darkTheme, lightTheme } from 'shared/theme'
 
+type ThemeType = 'light' | 'dark'
+
+const THEME_COOKIE_KEY = 'theme'
 export const useThemeToggler = () => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light')
+  const [theme, setTheme] = useState<ThemeType>(
+    Cookies.get(THEME_COOKIE_KEY)
+      ? (Cookies.get(THEME_COOKIE_KEY) as ThemeType)
+      : 'light',
+  )
 
   const toggleTheme = () => {
     setTheme(prevState => (prevState === 'light' ? 'dark' : 'light'))
   }
 
   const { time } = useGetUserTime()
+
+  useEffect(() => {
+    Cookies.set(THEME_COOKIE_KEY, theme, { expires: 1 })
+  }, [theme])
 
   useEffect(() => {
     const hours = time.getHours()
