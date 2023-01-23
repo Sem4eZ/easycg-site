890d6f54d0ac1f203bdcf78e4535859a7082d76d
Alina <ruesullivanpro@gmail.com>
2023-01-23 19:01:50 +0400

    chore: fix tags

--- src/shared/ui/tags/filter-tag.tsx
+++ src/shared/ui/tags/filter-tag.tsx
@@ -16,6 +16,7 @@ export const FilterTag = ({ children, active = false, ...rest }: Props) => {
 }
 
 const Container = styled(Button)<{ active: 0 | 1 }>(({ theme, active }) => ({
+  display: 'inline-flex',
   fontWeight: 700,
   border: `2px solid ${theme.palette.text.primary}`,
   backgroundColor: active ? theme.palette.text.primary : 'transparent',
@@ -27,16 +28,18 @@ const Container = styled(Button)<{ active: 0 | 1 }>(({ theme, active }) => ({
     paddingBottom: [10, null, null, null, null, 24],
     paddingLeft: [24, null, null, null, null, 48],
     paddingRight: [24, null, null, null, null, 48],
+    marginRight: [8, null, null, null, null, null, 16],
+    marginBottom: [16, null, 24, null, null, null, 32],
   }),
   '&:hover': {
     border: `4px solid ${theme.palette.text.primary}`,
     backgroundColor: active ? theme.palette.text.primary : 'transparent',
     color: active ? theme.palette.inverted : theme.palette.text.primary,
     ...getBreakpointsStylesByArray(theme, {
-      paddingTop: [10, null, null, null, null, 22],
-      paddingBottom: [10, null, null, null, null, 22],
-      paddingLeft: [24, null, null, null, null, 46],
-      paddingRight: [24, null, null, null, null, 46],
+      paddingTop: [8, null, null, null, null, 22],
+      paddingBottom: [8, null, null, null, null, 22],
+      paddingLeft: [22, null, null, null, null, 46],
+      paddingRight: [22, null, null, null, null, 46],
     }),
   },
   '& .MuiTouchRipple-root': {
