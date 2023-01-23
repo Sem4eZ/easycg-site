b63167f65cad3a8b34fb908dc72101bfd70d1cd3
Alina <ruesullivanpro@gmail.com>
2023-01-23 18:20:07 +0400

    chore: add project view picker

---
+++ src/entities/project/ui/use-project-view-picker.tsx
@@ -0,0 +1,48 @@
+import { styled } from '@mui/material/styles'
+import { useState } from 'react'
+
+import { getBreakpointsStylesByArray } from 'shared/lib/get-breakpoints-styles-by-array'
+import { useGetDevice } from 'shared/lib/use-get-device'
+import { Link } from 'shared/ui/link'
+
+type View = 'carousel' | 'list'
+
+export const useProjectViewPicker = () => {
+  const [view, setView] = useState<View>('carousel')
+  const {
+    isMobileS,
+    isMobileSLandscape,
+    isMobileLandscape,
+    isTablet,
+    isTabletLandscape,
+  } = useGetDevice()
+
+  return {
+    template: (
+      <Container>
+        <Link
+          active={view === 'carousel' ? 1 : 0}
+          onClick={() => setView('carousel')}>
+          {isMobileS ||
+          isMobileSLandscape ||
+          isMobileLandscape ||
+          isTablet ||
+          isTabletLandscape
+            ? 'scroll'
+            : 'carousel'}
+        </Link>
+        <Link active={view === 'list' ? 1 : 0} onClick={() => setView('list')}>
+          list
+        </Link>
+      </Container>
+    ),
+    view,
+  }
+}
+
+const Container = styled('div')(({ theme }) => ({
+  display: 'flex',
+  ...getBreakpointsStylesByArray(theme, {
+    gap: [32, 56, 32, 72, null, null, 80, null, 76, 80],
+  }),
+}))
